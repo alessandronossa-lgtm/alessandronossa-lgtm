b57b0ee87b0cbddc7235d16048dd871e5c5d6cd5
alessandronossa-lgtm <alessandro.nossa@gmail.com>
2026-02-17 18:18:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -192,4 +192,5 @@ def pendente():
 # ======================================
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
