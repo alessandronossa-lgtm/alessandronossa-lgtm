8a428e2a55a7ce6ae3c12c01ba7ccaae48d9f9aa
alessandronossa-lgtm <alessandro.nossa@gmail.com>
2026-04-22 17:06:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -545,8 +545,8 @@ def inserir_logo(ws):
     try:
         logo_path = os.path.join(app.root_path, "static", "logo.png")
         img = Image(logo_path)
-        img.width = 44
-        img.height = 44
+        img.width = 60
+        img.height = 60
         ws.add_image(img, "A2")
     except Exception as e:
         print("Erro ao inserir logo:", e)
